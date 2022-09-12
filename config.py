d40cf2927a5b537a22752ced8b3a7d2c43d6efde
aleyakh <aleyakh@yandex.ru>
2022-09-12 18:11:03 +1000

    HW17 done1

---
+++ config.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['RESTX_JSON'] = {'ensure_ascii': False}
+db = SQLAlchemy(app)
